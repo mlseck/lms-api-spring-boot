c77feeeafcbcf5ca6780ae97402eccb180ad9519
mlseck <mseck17@gmail.com>
2017-07-11 16:13:25 -0400

    Added web config file for outside access

---
+++ src/main/java/com/gcit/lms/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.gcit.lms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
