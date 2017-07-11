5d57e4291d2f4fc1871740b509726613e3774824
mlseck <mseck17@gmail.com>
2017-07-11 15:28:40 -0400

    Added jdbc driver in application properties

--- src/main/java/com/gcit/lms/service/BookController.java
+++ src/main/java/com/gcit/lms/service/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,8 @@ import com.gcit.lms.entity.Book;
 import com.gcit.lms.entity.Genre;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class BookController {
 
 	@Autowired
